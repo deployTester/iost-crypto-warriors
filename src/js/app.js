df16e3d9e5c7f1f4f1d32f10bc41ca856dd885c6
Calios <995967851@qq.com>
2018-04-02 17:03:06 +0800

    add avatar generator

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,15 @@ App = {
     web3Provider: null,
     contracts: {},
 
-    init: function() {
+    init: function () {
         // Load legends.
-        $.getJSON('../legends.json', function(data) {
+        $.getJSON('../legends.json', function (data) {
             var legendsRow = $('#legendRow');
             var legendTemplate = $('#legendTemplate');
 
             for (i = 0; i < data.length; i++) {
+                var url = 'https://avatars.dicebear.com/v1/male/' + data[i].id + '/100.png'
+                legendTemplate.find('.legend-avatar').attr('src', url);
                 legendTemplate.find('.legend-name').text(data[i].name);
                 legendTemplate.find('.legend-level').text(data[i].level);
                 legendTemplate.find('.legend-level').text(data[i].level);
@@ -19,13 +21,13 @@ App = {
         return App.initWeb3();
     },
 
-    initWeb3: function() {
+    initWeb3: function () {
 
     }
 };
 
-$(function() {
-    $(window).load(function() {
+$(function () {
+    $(window).load(function () {
         App.init();
     });
 });
--- src/js/snippets.js
+++ src/js/snippets.js
@@ -22,3 +22,23 @@ myButton.onclick = function () {
     setUserName();
 }
 */
+
+// generate random avatars based on seed:
+// https://github.com/DiceBear/avatars
+
+var Avatars = require('@dicebear/avatars');
+var avatars = new Avatars(Avatars.SPRITE_SETS.male); // male, female, identicon
+
+avatars.create('custom-seed').then(function (avatar) {
+    // Resize avatar and get as png data url
+    let dataUrl = avatar.getPNG({
+        size: 200
+    });
+
+    // Get as jpeg data url with white background
+    let dataUrl = avatar.getJPEG({
+        background: 200
+    });
+    console.log("<><><><><><");
+    console.log(dataUrl);
+});
