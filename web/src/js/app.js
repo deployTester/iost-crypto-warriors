73d9cbb8d151851ea7588bd58f8f88ee0d162387
Calios <995967851@qq.com>
2018-04-05 19:23:58 +0800

    read abi data from compiled json

--- web/src/js/app.js
+++ web/src/js/app.js
@@ -1,22 +1,22 @@
 App = {
     web3Provider: null,
-    gameContracts: {},
+    contracts: {},
 
     init: function () {
-		// Load legends.
-		// TODO: should get data from server or chain...
-        $.getJSON('../data/legends.json', function (data) {
-            var legendsRow = $('#legendRow');
-            var legendTemplate = $('#legendTemplate');
+        // Load fighters.
+        // TODO: should get data from server or chain...
+        $.getJSON('../data/fighters.json', function (data) {
+            var fightersRow = $('#fighterRow');
+            var fighterTemplate = $('#fighterTemplate');
 
             for (i = 0; i < data.length; i++) {
                 var url = '../images/avatar/boy2.jpg'; // + data[i].id;
                 var level = 'Level ' + data[i].level;
-                legendTemplate.find('.legend-avatar').attr('src', url);
-                legendTemplate.find('.legend-name').text(data[i].name);
-                legendTemplate.find('.legend-level').text(level);
-                legendTemplate.find('.legend-other').text(data[i].level);
-                legendsRow.append(legendTemplate.html());
+                fighterTemplate.find('.fighter-avatar').attr('src', url);
+                fighterTemplate.find('.fighter-name').text(data[i].name);
+                fighterTemplate.find('.fighter-level').text(level);
+                fighterTemplate.find('.fighter-other').text(data[i].level);
+                fightersRow.append(fighterTemplate.html());
             }
         });
 
@@ -28,54 +28,62 @@ App = {
             console.log('web3 exists! ');
             App.web3Provider = web3.currentProvider;
         } else {
+            // TODO: web3.js 1.0 prefers to a ws provider, while Metamask doesn't support it yet. 
+            // Update required later.            
+            // var web3Infura = new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws"));        
+            // var czEvents = new web3Infura.eth.Contract(cryptoZombiesABI, cryptoZombiesAddress);
             App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
         }
 
         web3 = new Web3(App.web3Provider);
-        console.log(web3);
-        // TODO: web3.js 1.0 prefers to a ws provider, while Metamask doesn't support it yet. 
-        // Update required later.
-        // var web3Infura = new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws"));        
-        // var czEvents = new web3Infura.eth.Contract(cryptoZombiesABI, cryptoZombiesAddress);
         return App.initContract();
     },
 
     initContract: function () {
         // 🚀：Artifacts are information about our contract such as its deployed address and Application Binary Interface (ABI). 
         // The ABI is a JavaScript object defining how to interact with the contract including its variables, functions and their parameters.
-        $.getJSON('../ethereum/build/contracts/Game.json', function (data) {
+        // TODO: use json for test.
+        $.getJSON('../data/Fighter.json', function (data) {
             // Get the necessary contract artifact file and instantiate it with truffle-contract
-            var GameArtifact = data;
-            App.gameContracts.Adoption = TruffleContract(GameArtifact);
+            var FighterArtifact = data;
+            // console.log(data);
+            App.contracts.Fighter = contract(FighterArtifact);
 
             // Set the provider for our contract
-            App.gameContracts.Adoption.setProvider(App.web3Provider);
+            App.contracts.Fighter.setProvider(App.web3Provider);
 
-            // Use our contract to retrieve and mark the adopted pets
-            return App.markAdopted();
+            // Use our contract to retrieve your fighter if any
+            return App.getFighterData();
         });
 
         return App.bindEvents();
     },
 
     bindEvents: function () {
-        $(document).on('click', '#create-legend-save', App.createLegend);
+        $(document).on('click', '#create-fighter-save', App.createFighter);
     },
 
-	createLegend: function() {
-		console.log('createLegend called');
+    createFighter: function () {
+        console.log('createFighter called');
 
-		var name = $('#legend-name').val()
-		var gender = $('.btn-group > .btn.active').text()
-	
-		console.log(name, gender)
-		// TODO: call create legend functions in contract
-	}
+        var name = $('#fighter-name').val();
+        var gender = $('.btn-group > .btn.active').text();
 
+        console.log(name, gender)
+        // TODO: call create fighter functions in contract
+    },
+
+    getFighterData: function() {
+        // TODO: render a fighter and his data if any
+    },
+
+    startFight: function() {
+
+    }
 };
 
 $(function () {
-    $(window).load(function () {
+    $(window).on('load', function(){ 
         App.init();
     });
 });
