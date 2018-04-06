43f2229ba901a88d953e4b97362b9b6109371abb
Dark Dong <viva@163.com>
2018-04-06 23:39:24 +0800

    npm install web3 in local ethereum/test folder

--- ethereum/test/app.js
+++ ethereum/test/app.js
@@ -1,5 +1,15 @@
 console.log('test...');
 
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+console.log(web3);
+console.log(web3.version);
+web3.eth.getAccounts().then(accounts=> {
+    console.log(accounts[0]);
+});
+web3.eth.net.getId().then(console.log);
+web3.eth.getGasPrice().then(console.log);
+
 const fs = require('fs');
 const file = 'ethereum/build/contracts/Game.json';
 const json = JSON.parse(fs.readFileSync(file));
