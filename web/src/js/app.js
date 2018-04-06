9d673be073562e368265e1dda608052bde827552
Calios <995967851@qq.com>
2018-04-06 18:26:35 +0800

    update profile css and reimport truffle-contract

--- web/src/js/app.js
+++ web/src/js/app.js
@@ -24,16 +24,16 @@ App = {
     },
 
     initWeb3: function () {
-        if (typeof web3 !== 'undefined') {
-            console.log('web3 exists! ');
-            App.web3Provider = web3.currentProvider;
-        } else {
+        // if (typeof web3 !== 'undefined') {
+        //     console.log('web3 exists! ');
+        //     App.web3Provider = web3.currentProvider;
+        // } else {
             // TODO: web3.js 1.0 prefers to a ws provider, while Metamask doesn't support it yet. 
             // Update required later.            
             // var web3Infura = new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws"));        
             // var czEvents = new web3Infura.eth.Contract(cryptoZombiesABI, cryptoZombiesAddress);
             App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
-        }
+        // }
 
         web3 = new Web3(App.web3Provider);
         return App.initContract();
@@ -43,23 +43,31 @@ App = {
         // 🚀：Artifacts are information about our contract such as its deployed address and Application Binary Interface (ABI). 
         // The ABI is a JavaScript object defining how to interact with the contract including its variables, functions and their parameters.
         // TODO: pretend to get json from contract compiled abi json.
-        $.getJSON('../data/Fighter.json', function (data) {
-            // Get the necessary contract artifact file and instantiate it with truffle-contract
-            var FighterArtifact = data;
-            // console.log(data);
-            App.contracts.Fighter = TruffleContract(FighterArtifact);
+        // $.getJSON('../data/Fighter.json', function (data) {
+        //     // Get the necessary contract artifact file and instantiate it with truffle-contract
+        //     var FighterArtifact = data;
+        //     // console.log(data);
+        //     App.contracts.Fighter = TruffleContract(FighterArtifact);
 
-            // Set the provider for our contract
-            App.contracts.Fighter.setProvider(App.web3Provider);
+        //     // Set the provider for our contract
+        //     App.contracts.Fighter.setProvider(App.web3Provider);
 
-            // Use our contract to retrieve your fighter if any
-            return App.getFighterData(data);
+        //     // Use our contract to retrieve your fighter if any
+        //     return App.getFighterData(data);
+        // });
+
+        $.getJSON('../data/Game.json', function (data) {
+            var GameArtifact = data;
+
+            App.contracts.Game = TruffleContract(GameArtifact);
+            App.contracts.Game.setProvider(App.web3Provider);
         });
 
         return App.bindEvents();
     },
 
     bindEvents: function () {
+        console.log('bind click');
         $(document).on('click', '#create-fighter-save', App.createFighter);
     },
 
@@ -68,23 +76,55 @@ App = {
 
         var name = $('#fighter-name').val();
         var gender = $('.btn-group > .btn.active').text();
+        gender = $.trim(gender);
 
-        console.log(name, $.trim(gender))
+        console.log(name, gender);
         // TODO: call create fighter functions in contract
+
+        var gameInstance;
+
+        web3.eth.getAccounts(function (error, accounts) {
+            if (error) {
+                console.log(error);
+            }
+
+            var account = accounts[0];
+
+            App.contracts.Game.deployed().then(function (instance) {
+                gameInstance = instance;
+
+                return gameInstance.createFighter(name, gender, {
+                    from: account
+                });
+            });
+            // App.contracts.Adoption.deployed().then(function (instance) {
+            //     adoptionInstance = instance;
+
+            //     // Execute adopt as a transaction by sending account
+            //     return adoptionInstance.adopt(petId, {
+            //         from: account
+            //     });
+            // }).then(function (result) {
+            //     return App.markAdopted();
+            // }).catch(function (err) {
+            //     console.log(err.message);
+            // });
+        });
     },
 
-    getFighterData: function(data) {
+    getFighterData: function (data) {
         // TODO: render a fighter and his data if any
-        console.log(data);
+        console.log(data['abi'][0]);
+
     },
 
-    startFight: function() {
+    startFight: function () {
 
     }
 };
 
 $(function () {
-    $(window).on('load', function(){ 
+    $(window).on('load', function () {
         App.init();
     });
 });
